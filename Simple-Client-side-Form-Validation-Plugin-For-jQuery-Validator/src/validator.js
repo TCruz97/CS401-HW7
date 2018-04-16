59b17827f507aac170acdd6d0c117871f24bc434
Turner Cruz <turnercruz@u.boisestate.edu>
2018-04-16 15:49:30 -0600

    updated 401

--- Simple-Client-side-Form-Validation-Plugin-For-jQuery-Validator/src/validator.js
+++
@@ -1,9 +0,0 @@
-  function Validator(element, options) {
-    this.element = element;
-    this.$element = $(element);
-    this.options = $.extend(true, {}, Validator.DEFAULTS, $.isPlainObject(options) && options);
-    this.isCheckboxOrRadio = /checkbox|radio/.test(element.type);
-    this.value = null;
-    this.valid = true;
-    this.init();
-  }
--- Simple-Client-side-Form-Validation-Plugin-For-jQuery-Validator/src/variables.js
+++
@@ -1,3 +0,0 @@
-  var NAMESPACE = 'validator';
-  var EVENT_SUCCESS = 'success.' + NAMESPACE;
-  var EVENT_ERROR = 'error.' + NAMESPACE;
